3d6470eea09678d9766b5b92680dfb4a83fd739b
guxtavow <aquinoavilag@gmail.com>
2024-04-22 17:17:49 -0300

    Feat os.mkdir function

---
+++ README.md
@@ -0,0 +1,62 @@
+# WEBSCRAPPING 
+
+## SOBRE O PROJETO:
+A finalidade deste projeto é criar um webscrapping com a tentativa de automatizar mensalmente a coleta de dados de um especifico site.
+
+## UTILIZAÇÕES:
+Neste projeto, utilizei **Python** com algumas bibliotecas: 
+<b>
+- Selenium
+- Time
+- OS
+- Requests
+</b>
+
+## O APP ATÉ O MOMENTO APRESENTA:
+- Extração de dados
+- Seleção de botões e selects automáticos
+- Redirecionamento de local de download
+
+## INSTALAÇÃO
+`$ pip install selenium`
+`$ pip install request`
+
+## STATUS DO PROJETO:
+ <h3><b><font color="#e5e619">Em andamento.</font></b></h3>
+
+***
+
+<br />
+<br />
+
+<br />
+<br />
+
+
+# English Version:
+
+## ABOUT THE PROJECT:
+The purpose of this project is to create a webscraping tool aiming to automate the monthly data collection from a specific website.
+
+## USES:
+In this project, I used Python with some libraries:
+<b>
+- Selenium
+- Time
+- OS
+- Requests
+</b>
+
+## CURRENT FEATURES ON APP:
+- Data extraction
+- Automatic button and select elements selection
+- Redirecting download location
+
+## INSTALLATION
+`$ npm install`
+
+## GETTING STARTED
+`$ npm start`
+
+## PROJECT STATUS:
+ <h3><b><font color="#e5e619">Ongoing.</font></b></h3>
--- chrome.py
+++ chrome.py
@@ -1,15 +1,53 @@
 import requests
+import os
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import Select
 from selenium.webdriver.chrome.options import Options
+import shutil
 import time
 
+Nome_Pasta = "teste"
 
+
+def criar_pasta(origin, nome_pasta):
+    if not os.path.exists(nome_pasta):
+        nova_pasta = os.path.join(origin,nome_pasta)
+        os.makedirs(nova_pasta)
+        return nova_pasta
+    else:
+        count = 0
+        while True:
+            nova_pasta = f"{nome_pasta}{count}"
+            if not os.path.exists(nova_pasta):
+                os.makedirs(nova_pasta)
+                return nova_pasta
+            count += 1
+
+
+
+
+
+extensao = ".csv"
+
+def mover_arquivo():
+
+    origem = r"C:\Users\e_gustavoaa\Documents\projetos\webscrapping\\"
+    destino = r"C:\Users\e_gustavoaa\Documents\projetos\webscrapping\\" + criar_pasta(origem,Nome_Pasta)
+    
+    os.listdir(destino)
+    
+    for nome_arquivo in os.listdir(origem):
+        if nome_arquivo.endswith(extensao):
+            caminho_origem = os.path.join(origem,nome_arquivo)
+            caminho_destino = os.path.join(destino,nome_arquivo)
+            shutil.move(caminho_origem, caminho_destino)
+            print(f"Arquivo '{nome_arquivo} movido para {destino}.")
+            
 
 url = "http://www22.receita.fazenda.gov.br/inscricaomei/private/pages/relatorios/opcoesRelatorio.jsf" #site do webscrapping
 
-response = requests.get(url)
+response = requests.get(url) 
 
 if response.status_code == 200:  #se conseguir acessar o site (codigo igual a 200)
     opcoes_chrome = webdriver.ChromeOptions()   #definindo variavel de opções do chrome
@@ -22,7 +60,7 @@ if response.status_code == 200:  #se conseguir acessar o site (codigo igual a 20
 
     opcoes_chrome.add_experimental_option("prefs", prefs) #chamo as preferencias
 
-    driver = webdriver.Chrome( options=opcoes_chrome) #inicio o webdriver
+    driver = webdriver.Chrome(options=opcoes_chrome) #inicio o webdriver
     driver.get(url)
     time.sleep(3) #3 segundos para pagina carregar e executar outras funções
 
@@ -42,6 +80,9 @@ if response.status_code == 200:  #se conseguir acessar o site (codigo igual a 20
     exporta = driver.find_element(By.NAME, 'form:botaoExportarCsv')
     exporta.click()
     time.sleep(7)
+
+    
+    mover_arquivo()
 
     driver.quit() #Fecho a janela
 
