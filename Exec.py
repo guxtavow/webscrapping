01dbbc1f05bce53504a1d0f23f605f6683b25158
guxtavow <aquinoavilag@gmail.com>
2024-06-13 11:06:28 -0300

    fix Exec.py

--- Exec.py
+++ Exec.py
@@ -7,7 +7,9 @@ def Arquivos():
         {'arquivo': 'Atividade_Economica.py', 'diretorio': local},
         {'arquivo': 'CNAE-UF.py', 'diretorio': local},
         {'arquivo': 'Municipio.py', 'diretorio': local},
-        {'arquivo': 'Municipio2.py', 'diretorio': local}
+        {'arquivo': 'Municipio2.py', 'diretorio': local},
+        {'arquivo': 'Faixa_Etaria.py', 'diretorio': local}
+
     ]
 
     # Loop sobre cada arquivo e executá-lo em um subprocesso no diretório correspondente
